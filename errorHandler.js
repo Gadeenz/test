3baa947b1f6704333e0bc2b7414db5bfba21d204
Gadeenz <mohammedgadeen@gmail.com>
2021-07-04 10:44:22 +0200

    first commit

---
+++ errorHandler.js
@@ -0,0 +1,20 @@
+function success(req,res) {
+    console.log("in datat")
+    return res.json({
+        success:1,
+        data:data, 
+        msg:"operation done successfuly"
+    });
+}
+
+function fail(error) {
+    return ({
+        success:0,
+        msg:"operation failed"
+    });
+}
+
+module.exports = {
+    success:success,
+    fail:fail
+}
